5bf2333b2e18440ba2292b5c28499f95cc3628dc
Roland Herzog <roland.herzog@mathematik.tu-chemnitz.de>
2017-07-22 07:11:37 +0200

    added visualization of iterations when n = 2

--- test_gmres_l12inf.m
+++ test_gmres_l12inf.m
@@ -1,5 +1,5 @@
 % Set problem dimension
-n = 100;
+n = 2;
 
 % Output some information
 fprintf('-------------------------------------------------------------\n');
@@ -53,16 +53,26 @@ fprintf(' Problem size is n = %d.\n',n);
 % matrix
 U = randn(n,n); U = orth(U);
 V = randn(n,n); V = orth(V);
-S = 1 + 0.01 * randn(n,n);
-A = U * S * V';
+S = 1 + 0.01 * randn(n,1);
+A = U * diag(S) * V';
 b = randn(n,1);
 fprintf(' Non-symmetric problem with singular values close to 1.\n');
+
+
+% Generate a problem with a few distinct eigenvalues
+% U = randn(n,n); 
+% D = randn(n,1);
+% D = round(D,0); D(D==0) = 1;
+% A = U * diag(D) * inv(U);
+% b = randn(n,1);
+% fprintf(' Non-symmetric problem with %d distinct eigenvalues.\n',length(unique(D)));
+
 
 % Set options 
 options.norm = 'l2';
 options.norm = 'linf';
 options.norm = 'l1';
-options.preserve_zero_residual_components = 1;  % only meaningful when options.norm == 'l1'
+options.preserve_zero_residual_components = 0;  % only meaningful when options.norm == 'l1'
 options.zero_residual_threshold = 1e-6;         % only meaningful when options.norm == 'l1' and options.preserve_zero_residual_components == 1
 
 % Output some information
@@ -136,4 +146,31 @@ if (~isempty(history.lpiter))
 	xlabel('iter');
 end
 grid on
+
+% Produce a plot showing the objective function |b - A x|_*
+% in case n = 2
+figure(6); clf; hold on
+if (n == 2)
+	xmin = min(X(1,:)); xmax = max(X(1,:)); 
+	xmin = xmin - 0.3 * (xmax - xmin); 
+	xmax = xmax + 0.3 * (xmax - xmin);
+	ymin = min(X(2,:)); ymax = max(X(2,:));
+	ymin = ymin - 0.3 * (ymax - ymin); 
+	ymax = ymax + 0.3 * (ymax - ymin);
+	nx = 41;
+	[XX,YY] = meshgrid(linspace(xmin,xmax,nx),linspace(ymin,ymax,nx));
+	ZZ = repmat(b,1,length(XX(:))) - A*[XX(:)'; YY(:)'];
+	if (strcmpi(options.norm,'l1'))
+		f = sum(abs(ZZ));
+	elseif (strcmpi(options.norm,'l2'))
+		f = sqrt(sum(ZZ.^2));
+	elseif (strcmpi(options.norm,'linf'))
+		f = max(abs(ZZ));
+	end
+	f = reshape(f,nx,nx);
+	contour(XX,YY,f);
+	plot(X(1,:),X(2,:),'r-o','MarkerSize',10,'MarkerFaceColor','r');
+	title('Level lines of the objective, and iterates');
+	axis equal
+end
 
